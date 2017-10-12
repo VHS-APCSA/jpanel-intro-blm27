4efcc69689f5af31ffeb410400dff1714ce8dc01
littl <littl@DESKTOP-IK0GM3J.attlocal.net>
2017-10-11 20:12:54 -0700

    Finished assignment.

--- SimplePanel.java
+++ SimplePanel.java
@@ -19,10 +19,19 @@ public class SimplePanel extends JPanel
     public void paintComponent(Graphics g) 
     {
         super.paintComponent(g);
-        g.setColor(Color.red);
+        g.setColor(Color.blue);
         width = getWidth();
         height = getHeight();
-        g.drawLine(0, 0, width, height);
-        g.drawLine(0, height, width, 0);
+        g.drawLine(0, height/2, width, height/2);
+        g.drawLine(width/2, height, width/2, 0);
+        g.setColor(Color.green);
+        g.drawRect(125/2, height/2+62 , 125, 100);
+        g.setColor(Color.red);
+        g.fillRect(width/2+62, height/2+62 , 125, 100);
+        g.setColor(Color.orange);
+        g.drawOval(width/2+62, height/6, 125, 100);
+        g.setColor(Color.white);
+        g.fillOval(125/2, height/6 , 125, 100);
+        
     }
 }
